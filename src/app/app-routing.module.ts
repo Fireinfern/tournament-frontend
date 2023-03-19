f1fb93e3b171f8f698957bfc4cd99144c24ca98e
Sebastian Silva <sebastiansilvabenites@gmail.com>
2023-03-18 23:15:05 -0400

    Add Basic Nav Bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 import { TournamentsComponent } from './views/tournaments/tournaments.component';
 
 const routes: Routes = [
-  {path: '', component: TournamentsComponent}
+  { path: '', component: HomeComponent },
+  { path: 'tournaments', component: TournamentsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TournamentsComponent,
-    TournamentGridComponent
+    TournamentGridComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +30,9 @@ import { MatDividerModule } from '@angular/material/divider';
     BrowserAnimationsModule,
     MatTableModule,
     MatButtonModule,
-    MatDividerModule
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
