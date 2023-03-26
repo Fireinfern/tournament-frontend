1ebab828f7adcce9574504e89cc6fc1fc7953a46
Sebastian Silva <sebastiansilvabenites@gmail.com>
2023-03-25 23:36:14 -0400

    Add Delete Tournament Confirmation

--- src/app/components/tournament-grid/tournament-grid.component.ts
+++ src/app/components/tournament-grid/tournament-grid.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteConfirmationComponent } from 'src/app/dialogs/delete-confirmation/delete-confirmation.component';
 import { Tournament } from 'src/app/models/tournament';
 import { TournamentService } from 'src/app/services/tournament.service';
 
@@ -7,14 +9,31 @@ import { TournamentService } from 'src/app/services/tournament.service';
   templateUrl: './tournament-grid.component.html',
   styleUrls: ['./tournament-grid.component.css']
 })
-export class TournamentGridComponent{
+export class TournamentGridComponent {
 
   tournaments!: Tournament[];
 
   displayedColumns = ["displayName", "maxPlayers", "rounds", "actions"];
 
-  constructor(private tournamentService: TournamentService) {
-    tournamentService.getTournaments().subscribe(data => {
+  constructor(private tournamentService: TournamentService, public confirmDeleteDialog: MatDialog) {
+    this.getTournaments();
+  }
+
+  openDeleteConfirmationButton(tournament: Tournament) {
+    const confirmDialogRef = this.confirmDeleteDialog.open(DeleteConfirmationComponent,
+      {
+        data: {
+          tournament: tournament
+        }
+      });
+    confirmDialogRef.afterClosed().subscribe((result) => {
+      this.tournaments = this.tournaments.filter((tournament) =>
+        tournament._id != result);
+    })
+  }
+
+  getTournaments() {
+    this.tournamentService.getTournaments().subscribe(data => {
       this.tournaments = data;
     });
   }
