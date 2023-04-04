79c3fdb93dbd070b99977c5d0a16b9ff334f7b7d
respatica <respatica@hotmail.com>
2023-04-03 20:02:03 -0400

    validation of forms

--- src/app/dialogs/login/login.component.ts
+++ src/app/dialogs/login/login.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, NgForm, Validators } from "@angular/forms";
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { LoginService} from 'src/app/services/login.service';
-
+import { AuthService} from 'src/app/services/auth.service';
 import { Router } from "@angular/router";
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { LoginerrorComponent } from '../loginerror/loginerror.component';
+
+import { error } from 'jquery';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
-export class LoginComponent {
+export class LoginComponent  {
+  
 [x: string]: any;
   public username!: string;
   public password!: string;
@@ -23,15 +25,22 @@ export class LoginComponent {
   password: ['', Validators.required]
   
   });
-  constructor( private formBuilder: FormBuilder,private router: Router, private loginService: LoginService,private dialogRef: MatDialogRef< LoginComponent> ,private dialog: MatDialog ){
+  constructor(private authService: AuthService,private changeDetectorRef: ChangeDetectorRef, private formBuilder: FormBuilder,private router: Router, private loginService: LoginService,private dialogRef: MatDialogRef< LoginComponent> ,private dialog: MatDialog ){
   
   }
   get f(){
     return this.loginForm.controls;
   }
+
+  
+
+
+
   login() {
     this.loginService.authenticate(this.loginForm.value.userName as string, this.loginForm.value.password as string).subscribe({
-      error: () => {
+      
+      
+      error: (error) => {   console.log(error);
         const dialogRef = this.dialog.open(LoginerrorComponent, {
           width: '450px',
           data: {message: 'Authentication failed. Please try again.'}
@@ -40,15 +49,31 @@ export class LoginComponent {
         dialogRef.afterClosed().subscribe(result => {
           this.loginForm.reset();
         });
+
+        
     },
-      next: (response) => {
-        if (response.status == 202 && response.body != null && response.body.hasOwnProperty("token")) {
+      next: (response) => {  console.log(response.status);
+        if (response.status === 202 && response.body != null && response.body.hasOwnProperty("token")) {
           localStorage.setItem("tournament-manager-token", response.body["token"]);
+          console.log(response.status);
           this.dialogRef.close();
+          this.authService.isAuthenticated = true;
+          
+          this.changeDetectorRef.detectChanges();
           return;
         }
-        if (response.status == 418) {
+        if (response.status === 418) {
+          console.log('418');
+          console.log(response.status);
           console.log("error not all things full");
+          const dialogRef = this.dialog.open(LoginerrorComponent, {
+            width: '450px',
+            data: {message: 'Please fill the username and pasword. Please try again.'}
+          });
+    
+          dialogRef.afterClosed().subscribe(result => {
+            this.loginForm.reset();
+          });
         }
         return;
     }});
