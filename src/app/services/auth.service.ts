7bf1bb3cd275e0676dc6facf282419862dc905dd
Sebastian Silva <sebastiansilvabenites@gmail.com>
2023-04-05 22:55:46 -0400

    Add Basic Display Of Rounds

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,5 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   isAuthenticated: boolean = false;
-  constructor() { }
+  constructor() {
+    let token = localStorage.getItem('token');
+    if (token) this.isAuthenticated = true;
+  }
 }
