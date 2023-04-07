41cdedf0abca76c1694450d53ceccc2679f59756
respatica <respatica@hotmail.com>
2023-04-07 14:44:55 -0400

    ihqveifcygqejvc

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,12 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   isAuthenticated: boolean = false;
-<<<<<<< HEAD
-  constructor() { }
-=======
+
   constructor() {
     let token = localStorage.getItem('token');
     if (token) this.isAuthenticated = true;
   }
->>>>>>> 1d661c33157939da8e3a77ccc1a0e37540cb59b0
+
 }
